439e309320850711fb705332065d4f9a88bf23cc
Ray'En Lim <rayen2912@gmail.com>
2025-06-21 03:41:21 +0800

    test

--- MeetCoordinator.py
+++ MeetCoordinator.py
@@ -99,8 +99,6 @@ def schedule_reminder(bot, chat_id: int, meeting_datetime: datetime, meeting_sum
         return None
 
 
-
-
 def extract_time_from_summary(summary: str) -> str:
     """Extract time from meeting summary"""
     for line in summary.split('\n'):
@@ -510,14 +508,8 @@ async def process_availability(update: Update, chat_id: int):
                     new_lines.append(line)
             summary = "\n".join(new_lines)
 
-
-        # Save to DB
-        db = SessionLocal()
-        meeting = Meeting(chat_id=chat_id, summary=summary, meet_date=meet_date)
-        db.add(meeting)
-        db.commit()
-
          # Schedule reminder if we have both date and time
+        reminder_time = None
         time_str = extract_time_from_summary(summary)
         if meet_date and time_str:
             meeting_datetime = parse_meeting_datetime(meet_date, time_str)
@@ -526,6 +518,11 @@ async def process_availability(update: Update, chat_id: int):
                 if reminder_time:
                     summary += f"\n\n⏰ **Reminder set for {reminder_time.strftime('%A, %B %d at %I:%M %p')}**"
 
+        # Now save summary *with* the correct reminder string
+        db = SessionLocal()
+        meeting = Meeting(chat_id=chat_id, summary=summary, meet_date=meet_date)
+        db.add(meeting)
+        db.commit()
 
         sync_link = f"{os.getenv('DOMAIN_BASE_URL')}/login?telegram_id={update.effective_user.id}&meeting_id={meeting.id}"
         final_message = (
